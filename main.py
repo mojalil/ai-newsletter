e4e5d737cbcf3d95d7e6e5227b0332eff0f0ecf7
motypes <mohammed.jalil@gmail.com>
2024-03-10 14:11:51 +0800

    wip agent and main logic setup

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-# Set up agents
-# Set up tasks
-# Set up tools
+from crewai import Crew, Process
+from langchain_openai import ChatOpenAI
+from file_io import save_markdown
 
+from dotenv import load_dotenv
+load_dotenv()
+
+from agents import AINewsLetterAgents
+from task import AINewsLetterTasks
+
+
+agents = AINewsLetterAgents()
+tasks = AINewsLetterTasks()
+
+# Initialize the OpenAI GPT-4 language model
+OpenAIGPT4 = ChatOpenAI(
+    model="gpt-4"
+)
+
+# Instantiate the agents
+editor = agents.editor_agent()
+news_fetcher = agents.news_fetcher_agent()
+news_analyzer = agents.news_analyzer_agent()
+newsletter_compiler = agents.newsletter_compiler_agent()
+
+# Instantiate the tasks
+fetch_news_task = tasks.fetch_news_task(news_fetcher)
+analyze_news_task = tasks.analyze_news_task(news_analyzer, [fetch_news_task])
+compile_newsletter_task = tasks.compile_newsletter_task(
+    newsletter_compiler, [analyze_news_task], save_markdown)
+
+# Form the crew
+crew = Crew(
+    agents=[editor, news_fetcher, news_analyzer, newsletter_compiler],
+    tasks=[fetch_news_task, analyze_news_task, compile_newsletter_task],
+    process=Process.hierarchical,
+    manager_llm=OpenAIGPT4,
+    verbose=2
+)
